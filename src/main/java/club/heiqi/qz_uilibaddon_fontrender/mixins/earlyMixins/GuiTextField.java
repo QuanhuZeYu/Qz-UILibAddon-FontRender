d4b7ba755b312ce37981677149e40a01a67bf0f8
泉户泽雨 <1624910218@qq.com>
2025-05-08 13:14:34 +0800

    杂项整理

---
+++ src/main/java/club/heiqi/qz_uilibaddon_fontrender/mixins/earlyMixins/GuiTextField.java
@@ -0,0 +1,20 @@
+package club.heiqi.qz_uilibaddon_fontrender.mixins.earlyMixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(net.minecraft.client.gui.GuiTextField.class)
+public class GuiTextField {
+
+    @Inject(
+        method = "textboxKeyTyped",
+        at = @At("HEAD"),
+        cancellable = true,
+        remap = true
+    )
+    public void qz_uilibAddonFontRender$textboxKeyTyped(char p_146201_1_, int p_146201_2_, CallbackInfoReturnable<Boolean> ci) {
+
+    }
+}
